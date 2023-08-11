74068a75d93fe348af78a6597173c2687bb139ca
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-08-11 11:37:42 -0300

    Aula 11/08 While | For | While Do

--- 1 des/FPOO/Atividades while, for, do while/Atividade 6.c
+++ 1 des/FPOO/Atividades while, for, do while/Atividade 6.c
@@ -3,14 +3,10 @@ int main (){
 	int i;
 	printf("Digite um valor inteiro para i: ");
 	scanf("%d",&i);
-	do{
-		printf("Digite um numero maior que %d para terminar o programa:",i);
+	while(i != 14){
+		printf("Digite um numero diferente para terminar o programa:	",i);
 		scanf("%d",&i);
-	}while(i < 14);
-	do{
-		printf("Digite um numero menor que %d para terminar o programa:",i);
-		scanf("%d",&i);
-	}while (i > 14);
+	}
 	if(i == 14){
 		printf("Parabens, voce acertou o numero! o numero era %d",i);
 		return 0;
--- 1 des/FPOO/Atividades while, for, do while/Atividade 7.c
+++ 1 des/FPOO/Atividades while, for, do while/Atividade 7.c
@@ -3,14 +3,10 @@ int main (){
 	int i;
 	printf("Digite um valor inteiro para i: ");
 	scanf("%d",&i);
-	do{
-		printf("O dobro de %d = %d\ndigite um numero maior para terminar o programa:	",i ,i*2);
+	while(i != 14){
+		printf("O dobro de %d = %d\ndigite um numero diferente para terminar o programa:	",i ,i*2);
 		scanf("%d",&i);
-	}while(i < 14);
-	do{
-		printf("O dobro de %d = %d\ndigite um numero menor para terminar o programa:	",i,i*2);
-		scanf("%d",&i);
-	}while (i > 14);
+	}
 	if(i == 14){
 		printf("Parabens, voce acertou o numero! o numero era %d",i);
 		return 0;
