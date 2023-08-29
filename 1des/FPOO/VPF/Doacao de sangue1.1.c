30922f9700d9038a714cf17532cb8f2c9c1df13a
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-08-29 09:20:21 -0300

    Revisão prova

--- 1des/FPOO/VPF/Doacao de sangue1.1.c
+++ 1des/FPOO/VPF/Doacao de sangue1.1.c
@@ -2,50 +2,68 @@
 #include <locale.h>
 int main(){
 	setlocale(LC_ALL,"");
-	//Declaração de variáveis
-	char nome[30];
-	int i, doadores, idade, sexo, dias, continuar;
+	int i, doadores, continuar;
 	
 	do{
 		printf("Quantos doadores tiveram no dia? ");
 		scanf("%d",&doadores);
+		char nome[doadores][30];
+		int idade[doadores], dias[doadores], sexo[doadores];
 		
 		for(i = 0; i < doadores; i++){
-			printf("Qual o nome do paciente? ");
-			scanf("%s", &nome);
-			printf("Qual o sexo? 1.Masculino 2.Feminino: ");
-			scanf("%d",&sexo);
+			printf("Qual o nome do %d° doador? ", i + 1);
+			scanf("%s", &nome[i]);
+			printf("Qual o sexo? Masculino(1) ou Feminino(2): ");
+			scanf("%d",&sexo[i]);
 			printf("Quantos anos tem? ");
-			scanf("%d",&idade);
+			scanf("%d",&idade[i]);
 			printf("Quanto tempo faz desde a última doação de sangue em dias? ");
-			scanf("%d",&dias);
-			switch(sexo){
+			scanf("%d",&dias[i]);
+		}
+		for(i = 0; i < doadores; i++){
+			char genero;
+			switch(sexo[i]){
 				case 1:
-					printf("%s é um homem de %d anos e que faz %d dias que não faz doação de sangue, logo:\n\n\n",nome, idade, dias);
-					if(idade < 16 || idade > 69 || dias < 60){
-						printf("%s está INAPTO\n",nome);
-						printf("A doação de sangue só é permitida a partir de 16 anos com a permissão dos pais com uma idade máxima de 69 anos\n");
-						printf("Também é necessário um tempo de 60 dias entre uma doação de sangue e outra\n\n");
-					}
-					if(idade >= 16 && idade <= 69 && dias >= 60){
-						printf("%s está APTO para doar sangue, obrigado pela cooperação\n\n",nome);
-					}
+					genero = 'M';
 				break;
 				case 2:
-					printf("%s é uma mulher de %d anos e que faz %d dias que não faz doação de sangue, logo:\n\n\n",nome, idade, dias);
-					if(idade < 16 || idade > 69 || dias < 90){
-						printf("%s está INAPTA\n",nome);
-						printf("A doação de sangue só é permitida a partir de 16 anos com a permissão dos pais com uma idade máxima de 69 anos\n");
-						printf("Também é necessário um tempo de 90 dias entre uma doação de sangue e outra\n\n");
-					}
-					if(idade >= 16 && idade <= 69 && dias >= 90){
-						printf("%s está APTA para doar sangue, obrigado pela cooperação\n\n",nome);
-					}
+					genero = 'F';
 				break;
 				default:
-					printf("Por favor insira 1 ou 2 para o sexo\n\n");
-			}
-	}
+					printf("Escreva 1 ou 2 para o sexo por favor\n");
+					continue;
+				}
+					
+				if(genero == 'M'){
+				
+					if(idade[i] < 16 || idade[i] > 69 || dias[i] < 60){
+						printf("\nInformações\n");
+						printf("Nome: %s\nIdade: %d\nSexo: %c\nDias desde a última doação: %d\n",nome[i], idade[i], genero, dias[i]);
+						printf("INAPTO!!\n");
+					}
+					else{
+						printf("\nInformações\n");
+						printf("Nome: %s\nIdade: %d\nSexo: %c\nDias desde a última doação: %d\n",nome[i], idade[i], genero, dias[i]);
+						printf("APTO!!\n");
+					}
+				}
+				if(genero == 'F'){
+					
+					if(idade[i] < 16 || idade[i] > 69 || dias[i] < 90){
+						printf("\nInformações\n");
+						printf("Nome: %s\nIdade: %d\nSexo: %c\nDias desde a última doação: %d\n",nome[i], idade[i], genero, dias[i]);
+						printf("INAPTA!!\n");
+					}
+					else{
+						printf("\nInformações\n");
+						printf("Nome: %s\nIdade: %d\nSexo: %c\nDias desde a última doação: %d\n",nome[i], idade[i], genero, dias[i]);
+						printf("APTA!!\n");
+					}
+				}
+	
+		}
+		
+
 	
 	printf("\nDeseja prosseguir com a próxima lista? Digite 1 para continuar e 0 para sair\n");
 	scanf("%d",&continuar);
