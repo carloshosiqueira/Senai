b16ac69351a8fe1281dc4a3b96c49f5b3564dcaf
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-09-28 11:37:08 -0300

    Correçao Exercicio 2 e 3 de manipulação de arquivos e tentativa de fazer o 4 e 5

---
+++ 1des/FPOO/Aula13/Atividades/Atividade4/Atividade4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<locale.h>
+int quantidade, i;
+
+int main(){
+	printf("Quantos comandos foram dados? ");
+	scanf("%d",&quantidade);
+	
+	char direcao[quantidade]
+	
+	printf("Quais foram as direções? E (esquerda) ou D(Direita): ");
+	for(i = 0; i < quantidade. i++){
+		scanf("%c",&direcao);
+	}
+	
+	
+	return 0;
+	
+}
