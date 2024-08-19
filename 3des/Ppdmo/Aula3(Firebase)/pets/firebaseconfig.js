a1f61f1bc67094128f8f83b2b1fea4685eca50f2
carloshosiqueira <carlos.siqueira.des@gmail.com>
2024-08-19 11:15:27 -0300

    Introdução firebase e exemplos de documentação de projeto

---
+++ 3des/Ppdmo/Aula3(Firebase)/pets/firebaseconfig.js
@@ -0,0 +1,21 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwKpw3iqgop--s_LGPRIh19GnQEadV7ek",
+    authDomain: "pets-632fc.firebaseapp.com",
+    projectId: "pets-632fc",
+    storageBucket: "pets-632fc.appspot.com",
+    messagingSenderId: "328954938060",
+    appId: "1:328954938060:web:95b95e25e99ac44550b869"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
+  const auth = getAuth(app);
+
+    export {db, auth};
